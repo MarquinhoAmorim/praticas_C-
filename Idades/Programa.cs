7dcf57d216acbf1743738eb90e78df242b61c94f
MarquinhoAmorim <marcoamorim877@gmail.com>
2024-03-01 23:08:56 -0300

    Atividades em C#

---
+++ Idades/Programa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace Programa
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            // Primeira Pessoa
+
+            Console.WriteLine("Dados da Primeira pessoa: ");
+
+            string nome1;
+            Console.Write("Nome: ");
+            nome1 = Console.ReadLine();
+
+            int idade1;
+            Console.Write("Idade: ");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            // Segunda Pessoa
+
+            Console.WriteLine("Dados da Segunda pessoa: ");
+
+            string nome2;
+            Console.Write("Nome: ");
+            nome2 = Console.ReadLine();
+
+            int idade2;
+            Console.Write("Idade: ");
+            idade2 = int.Parse(Console.ReadLine());
+
+            double media;
+            media = (double) (idade1 + idade2) / 2;
+
+            Console.WriteLine("A idade media de " + nome1 + " e " + nome2 + " eh de " + media.ToString("F1", CI) + " anos");
+
+
+        }
+    }
+}
---
+++ Retangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double b, a, area, perimetro, diagonal;
+
+            Console.Write("Base do Retangulo: ");
+            b = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Altura do Retangulo: ");
+            a = double.Parse(Console.ReadLine(), CI);
+
+            area = b * a;
+            perimetro = (a*2) + (b*2);
+            diagonal = Math.Sqrt((a*a)+(b*b));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CI));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CI));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
+        }
+    }
+}
